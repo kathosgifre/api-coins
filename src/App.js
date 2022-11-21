2e0d00ab1ca592febac3e955165ac1c8cfcd8063
kathosgifre <kathosgifre@gmail.com>
2022-11-21 16:26:44 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import TableCoin from "./components/TableCoin";
 
 function App() {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const getData = async () => {
+    try {
+      const res = await axios.get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      );
+      setCoins(res.data);
+      console.log(res.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <input
+          type="text"
+          placeholder="Search a Coin"
+          className="form-control bg-dark text-light border-0 mt-4 text-center"
+          autoFocus
+          onChange={(e) => setSearch(e.target.value)}
+        />
+
+        <TableCoin coins={coins} search={search} />
+      </div>
     </div>
   );
 }
+
 
 export default App;
